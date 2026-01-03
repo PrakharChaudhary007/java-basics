d139c3b4439227b08e6c6e3f70a7d4327ed4d5dd
PrakharChaudhary007 <8840518862sanket@gmail.com>
2026-01-03 22:13:40 +0530

    Add files via upload

---
+++ reverse.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class reverse {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n= 23597;
+        
+        int arr[];
+       while(n>0){
+        int  r = n%10;
+         n = n/10;
+         System.out.print(r);
+         }
+       }
+    }
+
