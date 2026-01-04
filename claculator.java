c2bb512039aff7120e41e24c806eae45566a9726
PrakharChaudhary007 <8840518862sanket@gmail.com>
2026-01-04 10:51:12 +0530

    03-01-2026

---
+++ claculator.java
@@ -0,0 +1,28 @@
+//import java.util.Scanner;
+//public class claculator {
+//    public static void main(String[] args) {
+//        Scanner input = new Scanner(System.in);
+//        System.out.print("enter the first number: ");
+//        int a = input.nextInt();
+//        System.out.print("enter the second number: ");
+//        int b = input.nextInt();
+//        System.out.println("1-Addition , 2-Substraction , 3- Division , 4-Multiplication");
+//        System.out.print("Which operation you want to perform: ");
+//        int operation = input.nextInt();
+
+//        if(operation ==1){
+//            System.out.println("the addition of two numbers is " + (a+b));
+
+//        }else if(operation==2){
+//            System.out.println("the substraction of numbers is "+ (a-b));
+
+//        }else if(operation==3){
+//            System.out.println("the Division of between two numbers is "+ (a/b));
+
+//        }else if(operation==4){
+//            System.out.println("the Multiplcation of two nummbers is "+ a*b);
+//        }
+//        }
+//    }
+// above program is for the two nummbers what if there is a multiple variables 
+
